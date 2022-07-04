777de4888d2af9c7841a74ae98f77a3b05ec5e38
schoenwetteradam <103909524+schoenwetteradam@users.noreply.github.com>
2022-07-04 11:14:42 -0500

    Add files via upload
    updated 7.4

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // selecting html elements
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu/classList.remove("active");
+}))
+
 let emailElement = document.querySelector('#email');
 let messageElement = document.querySelector('#message');
 
